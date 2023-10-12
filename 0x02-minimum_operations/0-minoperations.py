20c1a75cbb1cdba19192b3781e154c1c2df47da7
Webamz <chriszxn99@gmail.com>
2023-10-12 15:07:12 +0200

    minoperations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    """
+    Returns the fewest number of operations needed to result in exactly
+    n H characters in the file.
+    """
+    ops = 0
+    min_ops = 2
+    while n > 1:
+        while n % min_ops == 0:
+            ops += min_ops
+            n /= min_ops
+        min_ops += 1
+    return ops
